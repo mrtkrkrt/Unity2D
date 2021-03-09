074a80e5249a84def3e2d7b3365e28cdcf97dc5d
Murat Karakurt <55550212+mrtkrkrt@users.noreply.github.com>
2021-03-09 11:35:30 +0300

    2D

---
+++ Glitch Garden/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float fadeInTime;
+	
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	void Update () {
+		if (Time.timeSinceLevelLoad < fadeInTime) {
+			// Fade in
+			float alphaChange = Time.deltaTime / fadeInTime;
+			currentColor.a -= alphaChange;
+			fadePanel.color = currentColor;
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Glitch Garden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider volumeSlider, diffSlider;
+	public LevelManager levelManager;
+	
+	private MusicManager musicManager;
+
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
+		diffSlider.value = PlayerPrefsManager.GetDifficulty ();
+	}
+	
+	void Update () {
+		musicManager.SetVolume (volumeSlider.value);
+	}
+	
+	public void SaveAndExit () {
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+		PlayerPrefsManager.SetDifficulty (diffSlider.value);
+		levelManager.LoadLevel ("01a Start");
+	}
+	
+	public void SetDefaults () {
+		volumeSlider.value = 0.8f;
+		diffSlider.value = 2f;
+	}
+}
